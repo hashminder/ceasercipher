7f5ee495df7af02f36d93ef93b812b7612bb6e09
hashminder <irparsa2022@gmail.com>
2026-06-14 13:38:32 +0330

    0.3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use rand::Rng;
-
-fn random_bytes(len: usize) -> Vec<u8> {
-    let mut rng = rand::rng();
-    let mut v = vec![0u8; len];
-    rng.fill_bytes(&mut v);
-    v
-}
-
-
 fn encrypt (plaintext : String,key : u8) -> String {
     let shift_key: u8 = key % 126;
     let message = plaintext.as_bytes();
@@ -22,13 +12,38 @@ fn encrypt (plaintext : String,key : u8) -> String {
      return String::from_utf8(cipher).expect("failed to convert");
 }
 
+fn mod_backward (n : &u8,subtract : u8,mod_field : u8) -> u8 {
+    let go_forward = mod_field - (subtract % mod_field );
+     (n + go_forward) % mod_field
+}
+
+fn decrypt (cipher : String,key : u8) -> String {
+    let cipher = cipher.as_bytes();
+    let mut plain_text = Vec::new();
+    for i in cipher {
+        plain_text.push(i - key);
+    }
+    return String::from_utf8(plain_text).expect("failed to convert dec")
+}
+
+fn brute_force(cipher : String) -> Vec<String> {
+    let cipher = cipher.as_bytes();
+    let mut brute_array = Vec::new();
+    for i in 0..=255 {
+        let mut assemble = Vec::new();
+        for letter in cipher {
+            assemble.push(mod_backward(letter,i,127));
+        }
+        brute_array.push(String::from_utf8(assemble).expect("failed to convert the data"));
+    }
+    return brute_array
+}
 
 fn main() {
-    let mut i = 1;
-    loop {
-        let plaintext = "random_bytes(16)";
-        println!("{}",encrypt(plaintext.to_string(),i));
-        i += 1;
-        println!("{}",i);
-    }
+    let key = 3;
+    let plaintext = "some random text".to_string();
+    let c = encrypt(plaintext,key);
+    let _b = brute_force(c.clone());
+    println!("{:#?}",brute_force(c.clone()));
+    println!("{}",decrypt(c,key));
 }
